960aaaf76a866c996e59d606a11e3802d1ab3f28
astutox <38671208+astutox@users.noreply.github.com>
2020-06-04 23:21:19 -0500

    Agregada tabla de facturas en vista por cliente

--- ProyectoJPA/spring-boot-data-jpa/src/main/resources/import.sql
+++ ProyectoJPA/spring-boot-data-jpa/src/main/resources/import.sql
@@ -27,14 +27,21 @@ INSERT INTO clientes (nombre, apellido, email, create_at, foto) VALUES('Richard'
 
 
 /* Populate tabla productos */
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Panasonic Pantalla LCD',259990, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Sony Camara digital SC-W320B ,123490, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Apple iPod Shuffle ',1499990, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Sony Netbook Z110 ',259990, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Hewlett Packard Multifuncional F2280 ',69990, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Bianchi Bicicleta Aro 26',69990, NOW());
-INSERT INTO productos (nombre,precio,createAt) VALUES ('Mica Comoda 5 Cajones ',299990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Panasonic Pantalla LCD', 259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Camara digital SC-W320B', 123490, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Apple iPod Shuffle', 1499990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Sony Netbook Z110', 259990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Hewlett Packard Multifuncional F2280',69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Bianchi Bicicleta Aro 26', 69990, NOW());
+INSERT INTO productos (nombre, precio, create_at) VALUES('Mica Comoda 5 Cajones', 299990, NOW());
 
+/* Populate tabla facturas */
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura equipos de oficina', null, 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,1);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(2,1,4);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,5);
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(1,1,7);
 
-
+INSERT INTO facturas (descripcion, observacion, cliente_id, create_at) VALUES('Factura Bicicleta', 'Alguna nota importante!', 1, NOW());
+INSERT INTO facturas_items (cantidad, factura_id, producto_id) VALUES(3,2,6);
 
